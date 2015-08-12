97f81eb098b4d3df9e884eda4c47dcd657d1db88
Willis Zhang <williszhang@energysource-cn.com>
2015-08-12 11:55:15 +0800

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var cardsApp = angular.module('cardsApp', ['ngRoute']);
 
 cardsApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-		.when('/beckhu', {templateUrl: 'profiles/beckhu/beckhu.html'})	
-		
+		.when('/beckhu', {templateUrl: 'profiles/beckhu/beckhu.html'})
+		.otherwise({redirectTo: '/beckhu'});
 }]);
 
